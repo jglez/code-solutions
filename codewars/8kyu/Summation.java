d414ddaa15b10cd079d7410761e2fbf2e4f6b53c
Jorge Gonzalez <73510738+jglez@users.noreply.github.com>
2022-02-12 17:02:49 -0600

    Solves Summation
    This is an 8 Kyu code challenge from Codewars.
    The challenge is find the summation of a given int greater than zero.
    This solution is in Java.

---
+++ codewars/8kyu/Summation.java
@@ -0,0 +1,22 @@
+/*
+  Write a program that finds the summation of every number from 1 to n. The number will always be a positive integer greater than 0.
+
+  For example:
+    summation(2) -> 3
+    1 + 2
+
+    summation(8) -> 36
+    1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+*/
+
+public class Summation {
+  public static int summation(int n) {      
+    int sum = 0;
+    
+    for(int i = 1; i <= n; i++) {
+      sum = sum + i;
+    }
+    
+    return sum;
+  }
+}
