0c7ca3f93f001753eae7b2645b0a68121ef7001e
Jorge Gonzalez <73510738+jglez@users.noreply.github.com>
2022-04-21 19:51:38 -0500

    thirdAngle.py
    This is a 8kyu Kata from Codewars.
    
    Link to original problem: https://www.codewars.com/kata/5a023c426975981341000014

---
+++ codewars/8kyu/thirdAngle.py
@@ -0,0 +1,11 @@
+"""
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+"""
+
+def other_angle(angle1, angle2):
+    return 180 - angle1 - angle2
+  
