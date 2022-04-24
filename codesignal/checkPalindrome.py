88f84615357d6c974069182a18ef1a58f27caab9
Jorge Gonzalez <73510738+jglez@users.noreply.github.com>
2022-04-23 20:53:00 -0500

    Solves checkPalindrome
    This is a coding puzzle from CodeSignal's Arcade mode.
    This solution is in Python.

---
+++ codesignal/checkPalindrome.py
@@ -0,0 +1,19 @@
+"""
+Given the string, check if it is a palindrome.
+
+Example:
+    For inputString = "aabaa", the output should be
+    solution(inputString) = true
+    
+    For inputString = "abac", the output should be
+    solution(inputString) = false
+    
+    For inputString = "a", the output should be
+    solution(inputString) = true
+"""
+
+
+def solution(inputString):
+    if inputString == "".join(reversed(inputString)):
+        return True
+    return False
